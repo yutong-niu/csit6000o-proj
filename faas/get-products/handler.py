f6c8850984183122114cd6e7da77fc537488b957
root <root@ip-172-31-31-252.ap-southeast-1.compute.internal>
2022-05-08 15:57:33 +0000

    add faas get-products

---
+++ faas/get-products/handler.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+dir = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(dir, 'common/product_list.json'), 'r') as product_list:
+    product_list = json.load(product_list)
+
+HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+}
+
+def handle(req):
+    return {
+        "statusCode": 200,
+        "headers": HEADERS,
+        "body": json.dumps({"products": product_list}),
+    }
+
